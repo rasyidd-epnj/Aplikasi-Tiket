7203c441dedf36de0a2019f7695268a078fbad58
rasyidd-epnj <muhammadrasyid712@gamil.com>
2024-02-21 13:49:58 +0700

    Edited

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 alert(`Selamat Datang di Aplikasi Tiket, Mulailah`)
 
 let nama = prompt(`Masukkan Nama Anda`)
-let umur = prompt(`Masukkan Umur Anda`)
+let umur = parseInt(prompt(`Masukkan Umur Anda`))
 
 if (umur >= 13) {
     alert(`Anda Boleh Masuk Studio!`)
-  let studio = prompt(`Silahkan Pilih Studio \nA \nB \nC`)
+  let studio = prompt(`Silahkan Pilih Studio \nA \nB \nC`).toUpperCase()
 
   if (studio == `A`) {
     alert(`Tiket untuk ${nama} , Studio A, Umur ${umur} , Silahkan Masuk`)
